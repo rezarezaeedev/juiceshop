41a6e89532cbb4e4d3ec490e8b5458e7295cc8de
Reza Rezaee <rezarezaee.commercial@gmail.com>
2023-07-18 16:10:16 +0330

    add signal for automatic creating token for each new created users and set signal configs in apps.py in relaed django app folder.

--- Authentications/apps.py
+++ Authentications/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AuthenticationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Authentications'
+
+    
+    def ready(self):
+        from . import signals
