92d944ad9042e8fbbe18830f43310a42807b89c8
Reza Rezaee <rezarezaee.commercial@gmail.com>
2023-07-23 14:59:56 +0330

    change paignation style to offset style

--- JuiceShop_Root/settings.py
+++ JuiceShop_Root/settings.py
@@ -149,6 +149,6 @@ REST_FRAMEWORK = {
 
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 4,
 }
